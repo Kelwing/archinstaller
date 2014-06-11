dcd4059b45ad4f565308e39755e84a2de494ad54
Jacob Wiltse <aquabug@gmail.com>
2014-06-11 11:41:19 -0400

    Fixed password stdin

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ then
     chroot /mnt /bin/sh -c "locale-gen"
 
     # Set root passwd
-    chroot /mnt /bin/sh -c "echo $password | passwd --stdin"
+    chroot /mnt /bin/sh -c "echo $password\n$password | passwd --stdin"
 
     # Make initial ram disk
     chroot /mnt /bin/sh -c "mkinitcpio -p linux"
