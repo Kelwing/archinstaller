f4ab492d7338130233e7a65479d2fae6369b39f1
Jacob Wiltse <aquabug@gmail.com>
2014-09-02 11:23:46 -0400

    Added purpose to fullinstall.sh

--- examples/fullinstall.sh
+++ examples/fullinstall.sh
@@ -14,6 +14,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+# This example is designed to get you to a working desktop on a laptop
+
 ## Configuration Section
 
 # These are the package required for the installer to succeeed
